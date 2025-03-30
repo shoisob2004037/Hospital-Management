d9311a4770c8951bf573630183da08a88f993b70
Mahadi Hasan Shaisob <146670055+shoisob2004037@users.noreply.github.com>
2025-03-30 18:43:29 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,12 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
 
-// Configure cloudinary
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  api_key: process.env.CLOUDINARY_API_KEY,
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_secret: process.env.CLOUDINARY_API_SECRET, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
 });
 
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
-
-// Export the Express app for Vercel
-export default app;
+app.listen(process.env.PORT, () => { 
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
