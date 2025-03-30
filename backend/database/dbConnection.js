813b0af8be14028f786c2c52970de813a169a8b5
Mahadi Hasan Shaisob <146670055+shoisob2004037@users.noreply.github.com>
2025-03-30 18:43:49 +0600

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,26 +1,14 @@
 import mongoose from "mongoose";
 
-export const dbConnection = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
       dbName: "MERN_STACK_HOSPITAL_MANAGEMENT",
-      // These options help with connection stability
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB Database!");
+    })
+    .catch((err) => {
+      console.log("Some error Occured while connecting to database:", err);
     });
-    
-    console.log(`Connected to MongoDB Database: ${conn.connection.host}`);
-    
-    // Handle connection errors after initial connection
-    mongoose.connection.on('error', (err) => {
-      console.log('MongoDB connection error:', err);
-    });
-    
-    return conn;
-  } catch (err) {
-    console.log("Error connecting to database:", err);
-    // Don't crash the application, but log the error
-    return null;
-  }
-  
 };
